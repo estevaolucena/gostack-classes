1cfe4272f1cdff5f516914614b390455f8c821ca
Estevao Lucena <estevao.llucena@gmail.com>
2020-07-15 01:39:32 -0300

    Implements github api calls, handling errors, data storage

--- semana03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ semana03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,66 +1,98 @@
-import React from 'react'
-import { FiChevronRight } from 'react-icons/fi'
+import React, { useState, useEffect, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
-import logoImage from '../../assets/logo.svg'
+import logoImage from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório.');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Repositório não encontrado.');
+    }
+  }
+
   return (
     <>
-      <img src={logoImage} alt="Github Explorer"/>
+      <img src={logoImage} alt="Github Explorer" />
       <Title>Explore Repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório"/>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="">
-          <img
-            src="https://avatars2.githubusercontent.com/u/23317340?s=460&u=bc5552f62cbd03ceda228bbb0a4b2f3df3485dc9&v=4"
-            alt=""
-          />
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
+            <img src={repository.owner.avatar_url} alt="" />
 
-          <div>
-            <strong>estevaolucena/ecoleta</strong>
-            <p>Next Level Week 1.0: A complete application with NodeJS, ReactJS and React Native given by @Rocketseat :rocket:</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="">
-          <img
-            src="https://avatars2.githubusercontent.com/u/23317340?s=460&u=bc5552f62cbd03ceda228bbb0a4b2f3df3485dc9&v=4"
-            alt=""
-          />
-
-          <div>
-            <strong>estevaolucena/ecoleta</strong>
-            <p>Next Level Week 1.0: A complete application with NodeJS, ReactJS and React Native given by @Rocketseat :rocket:</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="">
-          <img
-            src="https://avatars2.githubusercontent.com/u/23317340?s=460&u=bc5552f62cbd03ceda228bbb0a4b2f3df3485dc9&v=4"
-            alt=""
-          />
-
-          <div>
-            <strong>estevaolucena/ecoleta</strong>
-            <p>Next Level Week 1.0: A complete application with NodeJS, ReactJS and React Native given by @Rocketseat :rocket:</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- semana03/primeiro-projeto-react/src/pages/Dashboard/styles.ts
+++ semana03/primeiro-projeto-react/src/pages/Dashboard/styles.ts
@@ -1,15 +1,19 @@
-import styled from 'styled-components'
-import { shade } from 'polished'
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
-  color: #3A3A3A;
+  color: #3a3a3a;
   max-width: 450px;
   line-height: 56px;
   margin-top: 80px;
-`
+`;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -20,20 +24,28 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
-    color: #3A3A3A;
+    color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
-      color: #A8A8B3
+      color: #a8a8b3;
     }
   }
 
   button {
     height: 70px;
     width: 210px;
-    background: #04D361;
+    background: #04d361;
     border-radius: 0 5px 5px 0;
     border: 0;
-    color: #FFF;
+    color: #fff;
     font-weight: bold;
     transition: background-color 0.2s;
 
@@ -41,7 +53,14 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04D361')};
     }
   }
-`
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`;
+
 export const Repositories = styled.div`
   margin-top: 80px;
   max-width: 700px;
@@ -58,7 +77,7 @@ export const Repositories = styled.div`
     transition: transform 0.2s;
 
     &:hover {
-      transform: translateX(10px)
+      transform: translateX(10px);
     }
 
     & + a {
@@ -72,23 +91,24 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      margin: 0 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
-        color: #3D3D4D;
+        color: #3d3d4d;
       }
 
       p {
         font-size: 18px;
-        color: #A8A8B3;
+        color: #a8a8b3;
         margin-top: 4px;
       }
     }
 
     svg {
       margin-left: auto;
-      color: #CBCBD6;
+      color: #cbcbd6;
     }
   }
-`
+`;
